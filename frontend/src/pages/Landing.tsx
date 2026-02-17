ce39dab3d116cb384864d581cd7aef89a552070d
Will Doan <pdoanthe96@gmail.com>
2026-02-16 22:14:59 -0500

    simple Hero Section

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,12 +1,39 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import styles from './Landing.module.css'
+
+const HeroSection = () => {
+  return (
+    <>
+      <div
+        className={styles.HeroSection}
+      >
+        <h1>
+          American Institute of Aeronautics and Astronauts at USF
+        </h1>
+        <h3>
+          Become part of our vibrant community
+        </h3>
+        <button
+          className={styles.CTAButton}
+        >
+          Join Now
+        </button>
+      </div>
+    </>
+  )
+}
 
 const Landing = () => {
   return (
     <>
-      <div>
+      <div
+        className={styles.Landing}
+      >
         <Navbar>
         </Navbar>
+        <HeroSection>
+        </HeroSection>
         <Footer>
         </Footer>
       </div>
