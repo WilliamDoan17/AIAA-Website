c8a2071b517cc6551326e8c1b710d477d24d546a
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-15 22:57:07 -0400

    feat: useEvents & useEvent

--- frontend/src/hooks/events.ts
+++ frontend/src/hooks/events.ts
@@ -1,11 +1,25 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAllEvents } from '../services/events.js'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { getAllEvents, getEventById } from '../services/events.js'
+import { EventInsert } from '../types/events.js'
 
 
 export const useEvents = () => {
   return useQuery({
     queryKey: ['events'],
     queryFn: getAllEvents,
+  })
+}
+
+export const useEvent = (id: string) => {
+  return useQuery({
+    queryKey: ['events', id],
+    queryFn: () => getEventById(id),
+  })
+}
+
+export const useCreateEvent = () => {
+  return useMutation({
+
   })
 }
 
